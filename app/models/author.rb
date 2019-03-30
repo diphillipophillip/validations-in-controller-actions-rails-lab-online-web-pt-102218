fb2288a98edb3e66b14e7ecde95322a80b1230c2
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-03-30 20:00:13 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ActiveRecord::Base
+<<<<<<< HEAD
     validates :email, uniqueness: true
     validates :name, presence: true
+=======
+  
+>>>>>>> 219ea2c8ff9fc2a86c1cd85a879c4a9453e9a271
 end
